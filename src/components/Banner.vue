0dec93e9c192c879b9421fdacfdd875cefdd5c2b
ricardoferreira68 <jrsf68@gmail.com>
2023-01-06 11:05:40 -0300

    Inicialização do Projeto

---
+++ src/components/Banner.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="main-banner">
+        <h1>Make Your Burger</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Banner"
+    
+}
+</script>
+
+<style scoped>
+#main-banner {
+    background-image: url("/public/img/burger.png");
+    background-position: 0;
+    background-size: cover;
+    height: 500px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+}
+
+#main-banner h1 {
+    color: #fff;
+    text-align: center;
+    font-size: 40px;
+    background-color: #222;
+    padding: 10px 20px;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <Banner />
+  <div class="main-container">
+    <h1>Monte o seu burger:</h1>
+    <Burgerform />
+  </div>
+</template>
+
+<script>
+import Banner from "../components/Banner.vue"
+import Burgerform from "../components/Burgerform.vue"
+
+export default {
+  nome: "Home",
+  components: {
+    Banner,
+    Burgerform
+  }
+}
+</script>
